c621bff350d19f3620e4a9474a36063f53cf4e0a
J Sai Bhagavan Reddy <116713860+SaiReddy513@users.noreply.github.com>
2023-03-18 18:45:05 +0530

    Add files via upload

---
+++ class auto.java
@@ -0,0 +1,21 @@
+class auto
+{
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=a*a;
+        while(a!=0)
+        {
+            if(a%10!=b%10)
+            {
+                System.out.print("false");
+
+            }
+            a=a/10;
+            b=b/10;
+        }
+        if()
+
+        
+    }
+}
